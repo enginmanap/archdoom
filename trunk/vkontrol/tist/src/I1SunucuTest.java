dcad9a590507c6ae7cf94ef94f2afa461cb51926
mustafasahin.mshn <mustafasahin.mshn@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-11-03 22:37:09 +0000

    Dosya okuma testinde, dosya varlığı testi eklendi.

--- trunk/vkontrol/tist/src/I1SunucuTest.java
+++ trunk/vkontrol/tist/src/I1SunucuTest.java
@@ -1,12 +1,27 @@
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
 import org.junit.Before;
 
 
-public class I1SunucuTest {
+public class I1SunucuTest extends TestCase{
 
 	@Before
 	public void setUp() throws Exception {
+		super.setUp();
 	}
 
+	public void testDosyaOkuma(){
+		File dosya = new File("deneme.txt");
+		File dosya2 = new File("olmayan.dosya");
+		assertEquals(true, dosya.exists());
+		assertEquals(false, dosya2.exists());
+		
+		
+		
+	}
 }
