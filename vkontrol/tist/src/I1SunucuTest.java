1466eff9248181f07237c2c600e1fc741745d1e7
mustafasahin.mshn <mustafasahin.mshn@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-11-03 23:07:23 +0000

    dosya satira alindi, dogrumu kontrolleri yapılmali. satiri alma islemi baska dosyaya tasinabilir.

--- vkontrol/tist/src/I1SunucuTest.java
+++ vkontrol/tist/src/I1SunucuTest.java
@@ -1,7 +1,13 @@
 import static org.junit.Assert.*;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 
 import junit.framework.TestCase;
 
@@ -21,7 +27,30 @@ public class I1SunucuTest extends TestCase{
 		assertEquals(true, dosya.exists());
 		assertEquals(false, dosya2.exists());
 		
+		File klasor = new File("src");
+		assertEquals(true, dosya.isFile());
+		assertEquals(false, dosya2.isFile());
+		assertEquals(true, dosya.canRead());
+		assertEquals(true, klasor.canRead());
 		
+		//Dosyanin icinde ne yaziyor bakilacak
+		FileReader dosyaOkuyucu = null;
+		try {
+			dosyaOkuyucu = new FileReader(dosya);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		BufferedReader girisAkisi = new BufferedReader(dosyaOkuyucu);
 		
+		//DataInputStream dosyaakisi = new DataInputStream(dosya);
+		String satir = null;
+		try {
+			satir = girisAkisi.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// satir degiskeninde bir satir var artik.
 	}
 }
