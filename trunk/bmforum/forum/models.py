d52bf1c1b1f7b59a75cd632e13fc881bdc3989c6
mesutcank <mesutcank@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-11-08 23:32:18 +0000

    admin paneli gerekli degisiklikler yapildi

--- trunk/bmforum/forum/models.py
+++ trunk/bmforum/forum/models.py
@@ -2,20 +2,6 @@ from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
 
-class Entry(models.Model):
-	user = models.ForeignKey(User, related_name = "entry_author")
-	topic = models.ForeignKey(Topic, related_name = "topic_of_entry")
-	text = models.TextField(verbose_name = "icerik")
-	date = models.DateTimeField("date_submitted", default = datetime.now())
-	isHidden = models.BooleanField("entry_deleted?", default = false)
-	isVotable = models.BooleanField("entry_votable?", default = true)
-
-
-#FIXME edit should be a new class	
-	isEdited = models.BooleanField("entry_edited?", default = false)
-	editdate = models.DateTimeField("date_edited")
-	editBy = models.ForeignKey(User, related_name ="entry_editer")
-
 class TopicPriorities(models.Model):
 	name = models.CharField(max_length = 100)
 	
@@ -23,10 +9,26 @@ class TopicPriorities(models.Model):
 		return self.name
 
 class Topic(models.Model):
-	title = models.charField(max_length = 100, verbose_name = "baslik")
-	priority = models.ForeignKey(TopicPriorities, blank=false, null=false, verbose_name = "onem")
-	subTopic = models.ForeignKey(Topic,blank=true,null=true,verbose_name = "ait_oldugu_baslik")
+	title = models.CharField(max_length = 100, verbose_name = "baslik")
+	priority = models.ForeignKey(TopicPriorities, blank=False, null=False, verbose_name = "onem")
+	subTopic = models.ForeignKey('self', blank=True, null=True, verbose_name = "ait_oldugu_baslik")
 	owner = models.ForeignKey(User, related_name = "creater_of_topic")
+
+class Entry(models.Model):
+	user = models.ForeignKey(User, related_name = "entry_author")
+	topic = models.ForeignKey(Topic, related_name = "topic_of_entry")
+	text = models.TextField(verbose_name = "icerik")
+	date = models.DateTimeField("date_submitted", default = datetime.now())
+	isHidden = models.BooleanField("entry_deleted?", default = False)
+	isVotable = models.BooleanField("entry_votable?", default = True)
+
+
+#FIXME edit should be a new class	
+	isEdited = models.BooleanField("entry_edited?", default = False)
+	editdate = models.DateTimeField("date_edited")
+	editBy = models.ForeignKey(User, related_name ="entry_editer")
+
+
 	
 
 
---
+++ trunk/bmforum/member/admin.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from django.contrib import admin
+from bmforum.member.models import *
+
+admin.site.register(Member)
