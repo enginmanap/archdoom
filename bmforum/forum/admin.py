80e92bf9750190543a571da6e9f0662f98fc4e46
mesutcank <mesutcank@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-11-08 23:32:18 +0000

    admin paneli gerekli degisiklikler yapildi

---
+++ bmforum/forum/admin.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from django.contrib import admin
+from bmforum.forum.models import *
+
+admin.site.register(Entry)
+admin.site.register(TopicPriorities)
+admin.site.register(Topic)
