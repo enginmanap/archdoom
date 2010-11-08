75175400487d218750b411e3db93d336736bba92
enginmanap <enginmanap@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-11-08 22:34:37 +0000

    entry için model oluşturuldu

--- trunk/bmforum/forum/models.py
+++ trunk/bmforum/forum/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
@@ -9,12 +10,21 @@ class User(models.Model):
 
 class Entry(models.Model):
 	user = models.ForeignKey(User, related_name = "entry_author")
-	topic = models.ForeignKey(Topic, related_name = "entry_author")
+	topic = models.ForeignKey(Topic, related_name = "topic_of_entry")
 	text = models.TextField(verbose_name = "icerik")
+	date = models.DateTimeField("date_submitted", default = datetime.now())
+	isHidden = models.BooleanField("entry_deleted?", default = false)
+	isVotable = models.BooleanField("entry_votable?", default = true)
 
 
+#FIXME edit should be a new class	
+	isEdited = models.BooleanField("entry_edited?", default = false)
+	editdate = models.DateTimeField("date_edited")
+	editBy = models.ForeignKey(User, related_name ="entry_editer")
 
 class Topic(models.Model):
+
+
 
 
 
