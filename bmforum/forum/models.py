419d8b4d9918d8c504906d3331d3b2e4abaf0d39
mesutcank <mesutcank@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-11-09 11:29:54 +0000

    gezegen eklendi, ozel mesaj eklendi, kullanıcı uyarıları eklendi

--- bmforum/forum/models.py
+++ bmforum/forum/models.py
@@ -13,6 +13,7 @@ class Topic(models.Model):
 	priority = models.ForeignKey(TopicPriorities, blank=False, null=False, verbose_name = "onem")
 	subTopic = models.ForeignKey('self', blank=True, null=True, verbose_name = "ait_oldugu_baslik")
 	owner = models.ForeignKey(User, related_name = "creater_of_topic")
+	firstEntry = models.ForeignKey('Entry', related_name = "ilk_girdi", null=True, blank=True)
 
 class Entry(models.Model):
 	user = models.ForeignKey(User, related_name = "entry_author")
@@ -21,16 +22,14 @@ class Entry(models.Model):
 	date = models.DateTimeField("date_submitted", default = datetime.now())
 	isHidden = models.BooleanField("entry_deleted?", default = False)
 	isVotable = models.BooleanField("entry_votable?", default = True)
-
-
 #FIXME edit should be a new class	
 	isEdited = models.BooleanField("entry_edited?", default = False)
 	editdate = models.DateTimeField("date_edited", blank=True, null=True)
 	editBy = models.ForeignKey(User, related_name ="entry_editer", blank=True, null=True)
 
-
-	
-
-
-
-	
+class PrivateMessage(models.Model):
+	title = models.CharField(max_length = 100, verbose_name="mesaj_basligi")
+	text = models.TextField(verbose_name = "mesaj_icerigi")
+	date = models.DateTimeField(verbose_name = "mesaj_yollama_tarihi", default=datetime.now())
+	pmFrom = models.ForeignKey(User, related_name="mesaj_yollayan")
+	pmTo = models.ForeignKey(User, related_name="mesaj_alan")
---
+++ bmforum/planet/admin.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from django.contrib import admin
+from bmforum.planet.models import *
+
+admin.site.register(Planet)
+admin.site.register(BlogEntry)
