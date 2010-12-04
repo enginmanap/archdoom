f95501d6c3d6d2f990dc4f04e0ead876ba17d843
enginmanap <enginmanap@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-12-04 22:23:12 +0000

    Dosya sınıfı okunacakDosya ve Yazılacak dosya olarak ikiye ayrıldı.

--- trunk/vkontrol/tist/src/YazilacakDosya.java
+++ trunk/vkontrol/tist/src/YazilacakDosya.java
@@ -1,30 +1,17 @@
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 
-public class Dosya {
+public class YazilacakDosya {
 
-	private ObjectInputStream objeGirisAkisi;
+	
 	private ObjectOutputStream objeCikisAkisi;
 	
-	public Dosya(String dosyaYolu) {
-		FileInputStream dosyaGirisAkisi = null;
+	public YazilacakDosya(String dosyaYolu) {
 		FileOutputStream dosyaCikisAkisi = null;
-
-		try {
-			dosyaGirisAkisi = new FileInputStream(dosyaYolu);
-			objeGirisAkisi = new ObjectInputStream(dosyaGirisAkisi);
-		} catch (FileNotFoundException e) {
-			System.out.print("dosya okuyucu baslatilamadi. ");
-		} catch (IOException e) {
-
-			System.out.print("Obje giris akisi baslatilamadi. ");
-			e.printStackTrace();
-		}
+	
 		
 		try {
 			dosyaCikisAkisi = new FileOutputStream(dosyaYolu);
@@ -34,27 +21,12 @@ public class Dosya {
 		} catch (IOException e) {
 			System.out.print("Obje cikis akisi baslatilamadi. ");
 		}
-		
-	}
-
-	public void setReader(ObjectInputStream sahteOkuyucu) {
-		objeGirisAkisi = sahteOkuyucu;
 	}
 	
 	public void setWriter(ObjectOutputStream sahteYazici) {
 		objeCikisAkisi = sahteYazici;
 	}
 	
-	public String satirOku(){
-		try {
-			return objeGirisAkisi.readUTF();
-		} catch (IOException e) {
-			System.out.println("Utf akisi okunamadi");
-			return null;
-		}
-	}
-
-
 	public boolean satirYaz(String yazilacakSatir){
 		try {
 			objeCikisAkisi.writeUTF(yazilacakSatir);
@@ -64,6 +36,4 @@ public class Dosya {
 			return false;
 		}
 	}
-
-
 }
