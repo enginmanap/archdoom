3c5fce9bfb1e020c032d191635f5691d4251d348
enginmanap <enginmanap@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-12-18 23:56:58 +0000

    anasayfa için forum linki oluşturuldu.
    forum için:
    topic listesi oluşturup gösterme,
    topic için entry listesi oluşturup gösterme
    özellikleri eklendi.

--- bmforum/bmforum/forum/views.py
+++ bmforum/bmforum/forum/views.py
@@ -1,8 +1,16 @@
 # Create your views here.
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
 from bmforum.forum.models import *
 
-def topic(request):
-    entry_list = Entry.objects.all()
-    return render_to_response('forum/topicentry.html', {'entry_list': entry_list})
+def giris(request):
+    return render_to_response('base.html')
+
+def topicList(self):
+    topic_list = Topic.objects.all()
+    return render_to_response('forum/topiclist.html', {'topic_list':topic_list})
+
+def showTopic(request, topic_id, topic_name):
+    topic = get_object_or_404(Topic, pk = topic_id)
+    entry_list = Entry.objects.filter(topic = topic)
+    return render_to_response('forum/topicentry.html', {'entry_list': entry_list, 'topic':topic })
--- bmforum/bmforum/urls.py
+++ bmforum/bmforum/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -11,6 +12,7 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    (r'^$', 'bmforum.forum.views.giris'),
     (r'^admin/', include(admin.site.urls)),
     (r'^forum/', include('bmforum.forum.urls')),
 )
