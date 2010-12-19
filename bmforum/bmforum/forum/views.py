eca633ef08fc763967b6dc22cdc6db09c65b4f41
enginmanap <enginmanap@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-12-19 22:07:21 +0000

    üyeler için giriş çıkış ve yeni entry girme özellikleri eklendi

--- bmforum/bmforum/forum/views.py
+++ bmforum/bmforum/forum/views.py
@@ -7,10 +7,10 @@ from bmforum.forum.forms import EntryForm
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 def giris(request):
-    return render_to_response('base.html')
+    return render_to_response('base.html', context_instance=RequestContext(request))
 
 def topicList(request):
     topic_list = Topic.objects.all()
@@ -22,12 +22,22 @@ def showTopic(request, topic_id, topic_name):
         form = EntryForm(request.POST)
         if form.is_valid():
             entry = form.save(commit = False)
+            entry.member = get_object_or_404(Member, user = request.user)
+            entry.date = datetime.now()
+            entry.topic = get_object_or_404(Topic, pk = topic_id)
+            entry.text = form.cleaned_data['text']
+            entry.save()
+            return HttpResponseRedirect(reverse('bmforum.forum.views.showTopic', args = (topic_id ,topic_name)))
     else:
         topic = get_object_or_404(Topic, pk = topic_id)
         entry_list = Entry.objects.filter(topic = topic)
         form = EntryForm()
-        return render_to_response('forum/topicentry.html', {'entry_list': entry_list, 'topic':topic, "form":form,}, context_instance=RequestContext(request))
-
+        try:
+            user = get_object_or_404(Member, user = request.user)
+            print "user :",user.user.username
+            return render_to_response('forum/topicentry.html', {'entry_list': entry_list, 'topic':topic, "form":form,"user":user}, context_instance=RequestContext(request))
+        except:
+            return render_to_response('forum/topicentry.html', {'entry_list': entry_list, 'topic':topic, "form":form, }, context_instance=RequestContext(request))
 
 def auth(request):
     username = request.POST['username']
@@ -36,11 +46,15 @@ def auth(request):
     if user is not None:
         if user.is_active:
             login(request, user)
-            return HttpResponseRedirect(reverse('bmforum.forum.views.topiclist'))
+            print "login"
+            return HttpResponseRedirect(reverse('bmforum.forum.views.topicList'))
         else:
             # Return a 'disabled account' error message
-            return HttpResponseRedirect(reverse('bmforum.forum.views.topiclist'))
+            return HttpResponseRedirect(reverse('bmforum.forum.views.topicList'))
     else:
         # Return an 'invalid login' error message.
-        return HttpResponseRedirect(reverse('bmforum.forum.views.topiclist'))
-        
+        return HttpResponseRedirect(reverse('bmforum.forum.views.topicList'))
+    
+def deauth(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('bmforum.forum.views.topicList'))
