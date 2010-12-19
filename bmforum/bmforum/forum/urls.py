aa3360244446c9e8deff06429aa8bcf4972d8c6d
enginmanap <enginmanap@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-12-19 18:21:36 +0000

    forum a forms.py eklendi, entry icin form olusturuldu. kimlik dogrulama icin metod yazildi. base.html de ve topicentry.html de duzenlemeler yapildi

--- bmforum/bmforum/forum/urls.py
+++ bmforum/bmforum/forum/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.defaults import *
 from django.contrib.auth.views import login
 urlpatterns = patterns('bmforum.forum.views',
         url(r'^$', 'topicList', name='topicList'),
-        url(r'^/topicList/$','topicList', name='topicList'),
+        url(r'^topicList/$','topicList', name='topicList'),
         url(r'^(?P<topic_id>\d+)-(?P<topic_name>.*)/$','showTopic', name='showTopic'),
+        url(r'^auth/$','auth', name='auth'),
         )
