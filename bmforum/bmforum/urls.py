4a1394d3e62a492e59d7facb0d25bfb24f2e0b10
mesutcank <mesutcank@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-12-19 15:08:08 +0000

    urls.py ye media/ dizini eklendi, base.html ye diger linkler de eklendi

--- bmforum/bmforum/urls.py
+++ bmforum/bmforum/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-
+from os import path
+DOCUMENT_ROOT = path.abspath(path.dirname(__file__))
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -15,4 +16,5 @@ urlpatterns = patterns('',
     (r'^$', 'bmforum.forum.views.giris'),
     (r'^admin/', include(admin.site.urls)),
     (r'^forum/', include('bmforum.forum.urls')),
+    (r'^media/(.*)$', 'django.views.static.serve', {'document_root': '%s/media' % DOCUMENT_ROOT, 'show_indexes': True}),
 )
