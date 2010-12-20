b22ecccabf177594a378a28d7a6d770d6777d45b
enginmanap <enginmanap@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-12-20 00:03:13 +0000

    forum icin iki seviyeli başlık listesine geçildi. başlık silme yetkisine göre başlık oluşturma izni verilecek.

--- bmforum/bmforum/forum/urls.py
+++ bmforum/bmforum/forum/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.defaults import *
 from django.contrib.auth.views import login
 urlpatterns = patterns('bmforum.forum.views',
         url(r'^$', 'topicList', name='topicList'),
-        url(r'^topicList/$','topicList', name='topicList'),
+        url(r'^topicList-(?P<topic_id>\d+)/$','topicList', name='topicList'),
         url(r'^(?P<topic_id>\d+)-(?P<topic_name>.*)/$','showTopic', name='showTopic'),
         )
--- bmforum/bmforum/forum/views.py
+++ bmforum/bmforum/forum/views.py
@@ -12,10 +12,14 @@ from django.contrib.auth import authenticate, login, logout
 def giris(request):
     return render_to_response('base.html', context_instance=RequestContext(request))
 
-def topicList(request):
-    topic_list = Topic.objects.all()
-    return render_to_response('forum/topiclist.html', {'topic_list':topic_list}, context_instance=RequestContext(request))
-
+def topicList(request, topic_id=0):
+    if topic_id == 0:
+        topic_list = Topic.objects.filter(subTopic = None )
+        return render_to_response('forum/topiclist.html', {'topic_list':topic_list,"root":True}, context_instance=RequestContext(request))
+    else:
+        topic = get_object_or_404(Topic, pk = topic_id)
+        topic_list = Topic.objects.filter(subTopic = topic )
+        return render_to_response('forum/topiclist.html', {'topic_list':topic_list,}, context_instance=RequestContext(request))
 
 def showTopic(request, topic_id, topic_name):
     if request.POST:
