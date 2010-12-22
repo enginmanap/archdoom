fafe04b7d82e9989711565bbfa5e60ba2e86b0c4
enginmanap <enginmanap@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-12-22 12:54:54 +0000

    entry edit eklendi.

--- trunk/bmforum/bmforum/forum/urls.py
+++ trunk/bmforum/bmforum/forum/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('bmforum.forum.views',
         url(r'^topicList-(?P<topic_id>\d+)/$','topicList', name='topicList'),
         url(r'^(?P<topic_id>\d+)-(?P<topic_name>.*)/$','showTopic', name='showTopic'),
         url(r'^addTopic-(?P<topic_id>\d+)/$','addTopic', name='addTopic'),
+        url(r'^editEntry-(?P<entry_id>\d+)/$','editEntry', name='editEntry'),
         )
