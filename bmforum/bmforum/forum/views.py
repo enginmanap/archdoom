89b45cb9fe20d5c1605558ad06c06341663d8842
enginmanap <enginmanap@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-12-22 13:01:53 +0000

    sql sorgularını konsola düşürmek için gereken printler eklendi.

--- bmforum/bmforum/forum/views.py
+++ bmforum/bmforum/forum/views.py
@@ -8,6 +8,8 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth import authenticate, login, logout
+from django.db import connection
+
 
 def giris(request):
     return render_to_response('base.html', context_instance=RequestContext(request))
@@ -15,10 +17,12 @@ def giris(request):
 def topicList(request, topic_id=0):
     if topic_id == 0:
         topic_list = Topic.objects.filter(subTopic = None, isHidden=False )
+        print connection.queries
         return render_to_response('forum/topiclist.html', {'topic_list':topic_list,"root":True}, context_instance=RequestContext(request))
     else:
         topic = get_object_or_404(Topic, pk = topic_id)
         topic_list = Topic.objects.filter(subTopic = topic, isHidden=False )
+        print connection.queries
         return render_to_response('forum/topiclist.html', {'topic_list':topic_list, "topic_id":topic_id}, context_instance=RequestContext(request))
 
 def addTopic(request, topic_id):
@@ -52,10 +56,12 @@ def addTopic(request, topic_id):
                 entry.save()
                 topic.firstEntry = get_object_or_404(Entry, pk = entry.id)
                 topic.save()
+                print connection.queries
         return HttpResponseRedirect(reverse('bmforum.forum.views.topicList'))
     else:
         topicForm = TopicForm( prefix ='topicForm')
         entryForm = EntryForm( prefix ='entryForm')
+        print connection.queries
         return render_to_response('forum/addTopic.html', {'topicForm': topicForm, 'entryForm':entryForm,'topic_id':topic_id }, context_instance=RequestContext(request))
 
 def showTopic(request, topic_id, topic_name):
@@ -68,11 +74,13 @@ def showTopic(request, topic_id, topic_name):
             entry.topic = get_object_or_404(Topic, pk = topic_id)
             entry.text = form.cleaned_data['text']
             entry.save()
+            print connection.queries
             return HttpResponseRedirect(reverse('bmforum.forum.views.showTopic', args = (topic_id ,topic_name)),)
     else:
         topic = get_object_or_404(Topic, pk = topic_id)
         entry_list = Entry.objects.filter(topic = topic, isHidden=False).order_by('-date')
         form = EntryForm()
+        print connection.queries
         return render_to_response('forum/topicentry.html', {'entry_list': entry_list, 'topic':topic, "form":form, }, context_instance=RequestContext(request))
 
 def editEntry(request,entry_id):
@@ -90,10 +98,12 @@ def editEntry(request,entry_id):
             entry.editDate = datetime.now()
             entry.editBy = get_object_or_404(Member, user = request.user)
             entry.save()
+            print connection.queries
             return HttpResponseRedirect(reverse('bmforum.forum.views.showTopic', args = (entry.topic.id ,entry.topic.title)),)
     else:
         entry = get_object_or_404(Entry, pk=entry_id)
         form = EntryEditForm({'topic':entry.topic.id,'text':entry.text})
+        print connection.queries
         return render_to_response('forum/editEntry.html', {'form':form, 'entry_id':entry.id}, context_instance=RequestContext(request))
 
 def auth(request):
@@ -103,17 +113,22 @@ def auth(request):
     if user is not None:
         if user.is_active:
             login(request, user)
+            print connection.queries
             return HttpResponseRedirect(reverse('bmforum.forum.views.topicList'))
         else:
             error = 'disabled account'
+            print connection.queries
             return render_to_response('error.html', {'error': error})
     else:
         error = 'invalid login'
+        print connection.queries
         return render_to_response('error.html', {'error': error})
 
 def deauth(request):
     logout(request)
+    print connection.queries
     return HttpResponseRedirect(reverse('bmforum.forum.views.topicList'))
 
 def authlogin(request):
+    print connection.queries
     return render_to_response('auth.html', context_instance=RequestContext(request))
