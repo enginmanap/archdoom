85295ae22d213688ab81700f6a819702477e3b88
mesutcank <mesutcank@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-12-25 11:57:21 +0000

    Veritabanı bağlantı sınıfı oluşturuldu, Node sınıfı oluşturuldu.

---
+++ medyaindeksleme/src/Node.java
@@ -0,0 +1,20 @@
+
+public class Node {
+	private String filePath;
+	private String fileName;
+
+	public String getFilePath() {
+		return filePath;
+	}
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	
+
+}
