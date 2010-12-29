35aa2f09363d1d4b44f8fbe33b856c85843fd4fb
enginmanap <enginmanap@2f86ef3f-ae44-4a6f-a27e-40328bdaa75b>
2010-12-29 02:21:08 +0000

    Ağaç yapısına, verilen bir klasörün içeriğini gösterme özelliği eklendi. seçili elemanların listesi, relativePath gösterecek şekilde güncellendi.

--- trunk/medyaindeksleme/src/CheckTree.java
+++ trunk/medyaindeksleme/src/CheckTree.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
@@ -19,7 +20,8 @@ public class CheckTree extends JTree {
 		//super(createTreeNodes()[0]);
 		DefaultTreeModel model = (DefaultTreeModel) this.getModel();
 		System.out.println("root is :"+model.getRoot());
-		model.setRoot(createTreeNodes()[0]);
+		File de = new File("D:\\Scrubs");
+		model.setRoot(createTreeNodes(de));
 		System.out.println("root is :"+model.getRoot()+ " count : "+model.getChildCount(model.getRoot()));
 		System.out.println("count"+this.getComponentCount());		
 		//this.getComponentCount();
@@ -41,47 +43,36 @@ public class CheckTree extends JTree {
 	    	CheckNode node = (CheckNode) e.nextElement();
 	    	if (node.isSelected()) {
 	    		TreeNode[] nodes = node.getPath();
-	    		selectedList.add(nodes[0].toString());
+	    		String path = "";
+	    		path = nodes[0].toString();
 	    		for (int i = 1; i < nodes.length; i++) {
-	    			selectedList.add(nodes[i].toString());
+	    			path += File.separatorChar + nodes[i].toString();
 	    		}
+	    		selectedList.add(path);
 	    	}
 	    }
 	    return selectedList;
 	}
-	
-	private static CheckNode[] createTreeNodes() {
-		String[] strs = {"swing",     // 0
-	         "platf",     // 1
-	         "basic",     // 2
-	         "metal",     // 3
-	         "JTree",    // 4
-	    "platf2",     // 1
-	    "basic2",     // 2
-	    "metal2",     // 3
-	    "JTree2",    // 4
-	    "platf2",     // 1
-	    "basic2",     // 2
-	    "metal2",     // 3
-	    "JTree2"};    // 4
-	                                             
-	    CheckNode[] nodes = new CheckNode[strs.length];
-	    for (int i=0;i<strs.length;i++) {
-	      nodes[i] = new CheckNode(strs[i]); 
-	    }
-	    nodes[0].add(nodes[1]);
-	    nodes[1].add(nodes[2]);
-	    nodes[1].add(nodes[3]);
-	    nodes[0].add(nodes[4]);
-	    nodes[0].add(nodes[5]);
-	    nodes[2].add(nodes[6]);
-	    nodes[2].add(nodes[7]);
-	    nodes[2].add(nodes[8]);
-	    nodes[1].add(nodes[9]);
-	    nodes[1].add(nodes[10]);
-	    nodes[4].add(nodes[11]);
-	    nodes[4].add(nodes[12]);
-	    nodes[3].setSelected(true);
+
+	private CheckNode createTreeNodes(File eklenenKlasor) {
+		CheckNode nodes = new CheckNode(eklenenKlasor.getName());
+				File[] files = eklenenKlasor.listFiles();
+
+
+	  
+			 for(int i=0; i < files.length; i++){
+				 //if the file is directory, call the function recursively
+					 if(files[i].isDirectory()){
+						 nodes.add(createTreeNodes(files[i]));
+						 continue;
+					 }else{
+						 nodes.add(new CheckNode(files[i].getName()));
+					 }
+			 }
+			 
+			 for(int i=0; i < files.length; i++){
+				 System.out.println("files ["+i+"] = " + files[i].getAbsolutePath());
+			 }
 		return nodes;
 	}
 }
